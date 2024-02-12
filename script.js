6b0352b9f434ae10682bed8b6512e08d22f11551
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-02-12 22:38:22 +0530

    update

--- script.js
+++ script.js
@@ -116,7 +116,7 @@
 ///////////////////////////////////////
 // Coding Challenge #2
 
-/* 
+/*
 1. Re-create challenge 1, but this time using an ES6 class;
 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
@@ -196,7 +196,7 @@ GOOD LUCK 😀
 ///////////////////////////////////////
 // Coding Challenge #3
 
-/* 
+/*
 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
@@ -207,42 +207,71 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-const Car = function (name, speed) {
-  this.name = name;
-  this.speed = speed;
-}
+// const Car = function (name, speed) {
+//   this.name = name;
+//   this.speed = speed;
+// }
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.name}going at ${this.speed} km/h, with a charge of ${this.charge}%`)
-}
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`new speed = ${this.speed}`); // we also can access this.make property here
-}
-const EV = function (name, speed, charge) {
-  Car.call(this, name, speed);
-  this.charge = charge;
-}
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-}
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge -= 1;
-  console.log(`${this.name}going at ${this.speed} km/h, with a charge of ${this.charge}%`);
-}
-
-
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.name}going at ${this.speed} km/h, with a charge of ${this.charge}%`)
+// }
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`new speed = ${this.speed}`); // we also can access this.make property here
+// }
+// const EV = function (name, speed, charge) {
+//   Car.call(this, name, speed);
+//   this.charge = charge;
+// }
+// EV.prototype = Object.create(Car.prototype);
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// }
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge -= 1;
+//   console.log(`${this.name}going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+// }
 
 
-const tesla = new EV('Tesla', 120, 23)
-console.log(tesla);
-tesla.chargeBattery(90);
-tesla.accelerate();
-tesla.accelerate();
-tesla.accelerate();
-tesla.brake()
-tesla.accelerate();
-console.dir(tesla)
+
+
+// const tesla = new EV('Tesla', 120, 23)
+// console.log(tesla);
+// tesla.chargeBattery(90);
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.brake()
+// tesla.accelerate();
+// console.dir(tesla)
+
+class PersonCl {
+  constructor(fullname, email) {
+    this.fullname = fullname;
+    this.email = email;
+  }
+}
+class StudentCl extends PersonCl {
+  constructor(fullname, email, password) {
+    super(fullname, email)
+    // this.password = password
+
+  }
+  // set passwordChk(password) {
+  //   if (!password.includes(' ')) this._password = password;
+  //   else alert('Please enter 8 characters in password')
+  // }
+  // get passwordChk() {
+  //   return this._password;
+  // }
+  // introduce() {
+  //   console.log(`Hey my name is ${this.fullname} and my email is ${this.email}`)
+  // }
+}
+
+const nisha = new StudentCl('Nisha', 'nisha43201@gmail.com')
+// nisha.introduce();
+console.dir(StudentCl.__proto__ === PersonCl.__proto__)
+
